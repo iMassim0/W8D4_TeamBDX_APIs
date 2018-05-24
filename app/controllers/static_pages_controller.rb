191a5d53f0a927c1b8a45b7e722eb720f9543ab0
iMassim0 <i.max@live.fr>
2018-05-24 14:23:34 +0200

    google finito

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def index
+    @position = params_position
+
   end
+
+  private
+
+  def params_position
+    params_position = params.permit(:longitude, :latitude)
+  end
+
 end
